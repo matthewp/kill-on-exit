83cb1c44f050baea3f5862010055b405bbf35208
Matthew Phillips <matthew@matthewphillips.info>
2015-10-22 17:05:25 -0400

    Exit after killing the child

--- child.js
+++ child.js
@@ -7,5 +7,6 @@ function infanticide(child, signal, parent) {
   parent.on(signal, function onexit(){
     parent.removeListener(signal, onexit);
     child.kill(signal);
+    process.exit();
   });
 }
