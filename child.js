6cbadf96bba752fd2d767865fef1e1efcb8611f9
Adam Barrett <adamlbarrett@hotmail.com>
2017-06-08 22:25:10 -0600

    feat(uncaught-exceptions): kill the child proccess on parent uncaught exceptions
    #1

--- child.js
+++ child.js
@@ -4,9 +4,15 @@ function infanticide(child, signal, parent) {
   signal = signal || "SIGTERM";
   parent = parent || process;
 
-  parent.on(signal, function onexit(){
+  parent.on(signal, function onexit() {
     parent.removeListener(signal, onexit);
     child.kill(signal);
     process.exit();
+  });
+
+  parent.on("uncaughtException", function onUncaughtException(err) {
+    parent.removeListener("uncaughtException", onUncaughtException);
+    console.error("Uncaught Exception\n", err.stack);
+    child.kill(signal);
   });
 }
